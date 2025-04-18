43e1cb277d3653c2c41efd17323dbf9f4e7d721e
dilipkumar300888 <dilipkumar.300888@gmail.com>
2025-04-18 11:04:37 +0530

    Added searchByCapital,count,sorted-by-population GetMapping

--- src/main/java/org/example/countryinfoapi/controller/CountryController.java
+++ src/main/java/org/example/countryinfoapi/controller/CountryController.java
@@ -3,10 +3,7 @@ package org.example.countryinfoapi.controller;
 import org.example.countryinfoapi.model.Country;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/v1/countries")
@@ -32,6 +29,29 @@ public class CountryController {
     @DeleteMapping("/{code}")
     public String deleteCountry(@PathVariable("code") String code) {
         return countries.remove(code.toUpperCase()) != null ? "Deleted Successfully!" : "Country not found!";
+    }
+
+    @GetMapping("/search")
+    public List<Country> searchByCapital(@RequestParam String capital) {
+        List<Country> result = new ArrayList<>();
+        for (Country country : countries.values()) {
+            if(country.getCapital().equalsIgnoreCase(capital)) {
+                result.add(country);
+            }
+        }
+        return result;
+    }
+
+    @GetMapping("/count")
+    public int getCountryCount() {
+        return countries.size();
+    }
+
+    @GetMapping("/sorted-by-population")
+    public List<Country> sortedByPopulation() {
+        List<Country> sorted = new ArrayList<>(countries.values());
+        sorted.sort((a,b) -> Long.compare(b.getPopulation(), a.getPopulation()));
+        return sorted;
     }
 
 }
